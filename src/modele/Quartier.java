5a8ab5afb49546091ad67278be8f25f785786501
lpoizac <lucile.poizac.sn@gmail.com>
2023-11-27 09:45:58 +0100

    changed project organization started working on Quartier class

---
+++ src/modele/Quartier.java
@@ -0,0 +1,12 @@
+package modele;
+
+public class Quartier {
+
+    private String nom;
+    private String type;
+    private int coutConstruction;
+    private String caracteristiques;
+
+    public static final String[] TYPE_QUARTIERS = {"RELIGIEUX", "MILITAIRE", "NOBLE", "COMMERCANT", "MERVEILLE"};
+
+}
