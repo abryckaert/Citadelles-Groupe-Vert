1e9aa2ea68627932310e45f5fa1c357487486f07
lpoizac <lucile.poizac.sn@gmail.com>
2023-11-30 19:10:22 +0100

    Began working on the Pioche file

---
+++ src/modele/Pioche.java
@@ -0,0 +1,12 @@
+package modele;
+
+import java.util.ArrayList;
+
+public class Pioche {
+    private ArrayList<Quartier> listeQuartiers;
+
+    public Pioche()
+    {
+        // initialisation de l'arrayList listeQuartiers.
+    }
+}
