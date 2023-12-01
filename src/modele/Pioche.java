6fb7f8f36019a9374a8494cbd214188c4c24df48
lpoizac <lucile.poizac.sn@gmail.com>
2023-12-01 17:08:11 +0100

    Pioche file passed the tests. Now working on PlateauDeJeu.java

--- src/modele/Pioche.java
+++ src/modele/Pioche.java
@@ -1,12 +1,52 @@
 package modele;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class Pioche {
     private ArrayList<Quartier> listeQuartiers;
 
+    Random generateur = new Random();
+
     public Pioche()
     {
         // initialisation de l'arrayList listeQuartiers.
+        listeQuartiers = new ArrayList<>();
     }
+
+    public Quartier piocher()
+    {
+        Quartier cartePiochee = null;
+        if (nombreQuartiersDansPioche() > 0) {
+            cartePiochee = listeQuartiers.get(0);
+            listeQuartiers.remove(0);
+            return cartePiochee;
+        }
+        else if (nombreQuartiersDansPioche() == 0)
+        {
+            cartePiochee = null;
+        }
+        return cartePiochee;
+    }
+
+    public void ajouter(Quartier nouveau)
+    {
+        listeQuartiers.addLast(nouveau);
+    }
+
+    public int nombreQuartiersDansPioche()
+    {
+        return listeQuartiers.size();
+    }
+
+    public void melanger()
+    {
+        for (int i = 0; i < nombreQuartiersDansPioche(); i++) {
+            int k = generateur.nextInt(nombreQuartiersDansPioche());
+            int j = generateur.nextInt(nombreQuartiersDansPioche());
+            Collections.swap(listeQuartiers,j,k);
+        }
+    }
+
 }
